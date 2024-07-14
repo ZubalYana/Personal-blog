70164f1420d111aa1e39c05d7d595b8bfde8e750
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-14 23:11:16 +0300

    set up all the pages changing and style headers on all the pages

--- public/main.js
+++ public/main.js
@@ -3,23 +3,14 @@ $('.home').click(() => {
     $('.mainPage').css('display', 'flex');
     $('.blogPage').css('display', 'none');
     $('.helpPage').css('display', 'none');
-    // $('home').addClass('active');
-    // $('blog').removeClass('active');
-    // $('help').removeClass('active');
 });
 $('.blog').click(() => {
     $('.mainPage').css('display', 'none');
     $('.blogPage').css('display', 'flex');
     $('.helpPage').css('display', 'none');
-    // $('home').removeClass('active');
-    // $('blog').addClass('active');
-    // $('help').removeClass('active');
 });
-$('.hohelpe').click(() => {
+$('.help').click(() => {
     $('.mainPage').css('display', 'none');
     $('.blogPage').css('display', 'none');
     $('.helpPage').css('display', 'flex');
-    // $('home').removeClass('active');
-    // $('blog').removeClass('active');
-    // $('help').addClass('active');
 });
