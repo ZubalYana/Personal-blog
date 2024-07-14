344f5247f57e86a6ff386e9f7b25c8745449aba4
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-14 23:05:09 +0300

    fix the clicking mistake and add headers to the pages

--- public/main.js
+++ public/main.js
@@ -1,25 +1,25 @@
 //pages changing
-$('home').click(() => {
+$('.home').click(() => {
     $('.mainPage').css('display', 'flex');
     $('.blogPage').css('display', 'none');
     $('.helpPage').css('display', 'none');
-    $('home').addClass('active');
-    $('blog').removeClass('active');
-    $('help').removeClass('active');
+    // $('home').addClass('active');
+    // $('blog').removeClass('active');
+    // $('help').removeClass('active');
 });
-$('blog').click(() => {
+$('.blog').click(() => {
     $('.mainPage').css('display', 'none');
     $('.blogPage').css('display', 'flex');
     $('.helpPage').css('display', 'none');
-    $('home').removeClass('active');
-    $('blog').addClass('active');
-    $('help').removeClass('active');
+    // $('home').removeClass('active');
+    // $('blog').addClass('active');
+    // $('help').removeClass('active');
 });
-$('hohelpe').click(() => {
+$('.hohelpe').click(() => {
     $('.mainPage').css('display', 'none');
     $('.blogPage').css('display', 'none');
     $('.helpPage').css('display', 'flex');
-    $('home').removeClass('active');
-    $('blog').removeClass('active');
-    $('help').addClass('active');
+    // $('home').removeClass('active');
+    // $('blog').removeClass('active');
+    // $('help').addClass('active');
 });
