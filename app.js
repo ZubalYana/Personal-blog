c2571603a4c0511d5e890f1712a7615bf8ebd53c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-15 22:28:43 +0300

    connect and install all the auth required packages

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const PORT = 3000;
-const path = require('path')
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const path = require('path');
 const app = express();
+const PORT = 3000;
+const JWT_SECRET = 'your_jwt_secret';
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res)=>{
     res.sendFile(__dirname, 'public', 'index.html')
