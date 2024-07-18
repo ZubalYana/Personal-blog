ad5d94ee54a53af414bc638d74144bb07dc2fb57
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-18 11:05:14 +0300

    fix the endpoint and open the user page

--- app.js
+++ app.js
@@ -101,13 +101,18 @@ const authMiddleware = (req, res, next) => {
     }
 };
 
-//public endpoint 
 app.get('/auth', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'auth.html'));
 });
+
 app.get('/', (req, res)=>{
     res.sendFile(__dirname, 'public', 'index.html')
 })
+
+app.get('/user', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'user.html'));
+});
+
 app.listen(PORT, ()=>{
     console.log(`Server works on PORT: ${PORT}`)
 })
