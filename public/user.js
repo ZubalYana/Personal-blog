a5bf5208332397406f1a905467d8ebdc8f172afa
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-22 12:47:13 +0300

    finish basic user profile styles

--- public/user.js
+++ public/user.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 <h2 class="FistLastName">${res.data.firstname} ${res.data.lastName}</h2>
                 <p class="email">${res.data.email}</p>
                 <p class="description">${res.data.profileDescription}</p>
-                <p class="placesVisited">Visited: <p class='visitedPlaces'>${res.data.placesVisited}</p></p>
-                <p class="placesToVisit">Wants to visit: <p class='toVisitPlaces' >${res.data.placesToVisit}</p></p>
+                <span class="placesVisited">Visited: <p class='visitedPlaces'>${res.data.placesVisited}</p></span>
+                <span class="placesToVisit">Wants to visit: <p class='toVisitPlaces' >${res.data.placesToVisit}</p></span>
             </div>
             <div class="posts">
                 <div class="published"></div>
