7fb5d7562838921ebf712a2c31220b96e1d304a4
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-22 12:25:35 +0300

    append the user profile structure to the wrap

--- public/user.js
+++ public/user.js
@@ -8,5 +8,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
     axios.get('/auth/user')
 .then(res=>{
     console.log(res.data)
+    $('.wrap').append(
+        `
+                <div class="user">
+            <div class="userInfo">
+                <img class="userPicture" src="" alt="">
+                <div class="FistLastName"></div>
+                <div class="email"></div>
+                <div class="placesVisited">Visited:</div>
+                <div class="placesToVisite">Wants to visit:</div>
+                <div class="posts">
+                    <div class="published"></div>
+                    <div class="liked"></div>
+                </div>
+            </div>
+        </div>
+        `
+    )
 })
 })
