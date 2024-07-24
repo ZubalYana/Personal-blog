8bc78a97c2251dae5b601fcb2c3b3dbc26238ff6
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-24 14:16:28 +0300

    create a models folder and a post model

---
+++ models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const postSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
