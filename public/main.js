95f1e4b212cc4c5af510254e017e1b35e059e9b7
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-25 12:05:40 +0300

    display the date using moment.js

--- public/main.js
+++ public/main.js
@@ -86,31 +86,31 @@ $(document).ready(function() {
 //get and display all the posts
 axios.get('/api/getPosts')
 .then((res)=>{
-    console.log(res.data)
-    for( let post of res.data ){
+    console.log(res.data);
+    for (let post of res.data) {
+        const formattedDate = moment(post.date).fromNow();
         $('.postsContainer').append(
             `
             <div class="post">
-                        <div class="top">
-                            <div class="author">
-                                <img class="author_pic" src="${post.author.profilePicture}" alt="">
-                                <p class="authro_name">${post.author.firstname} ${post.author.lastName}</p>
-                                <div class="dot"></div>
-                                <p class="follow">follow</p>
-                            </div>
-                            <p class="time">${post.date}</p>
-                        </div>
-                        <img class="postImg" src="${post.pic}" alt="">
-                        <h3 class="postTitle">${post.title}</h3>
-                        <p class="postText">${post.body}</p>
-                        <p class="postHashtags">${post.hashtags}</p>
-                        <div class="actions">
-                            <i class="fa-regular fa-thumbs-up"></i>
-                            <i class="fa-solid fa-share-nodes"></i>
-                        </div>
+                <div class="top">
+                    <div class="author">
+                        <img class="author_pic" src="${post.author.profilePicture}" alt="">
+                        <p class="authro_name">${post.author.firstname} ${post.author.lastName}</p>
+                        <div class="dot"></div>
+                        <p class="follow">follow</p>
                     </div>
+                    <p class="time">${formattedDate}</p>
+                </div>
+                <img class="postImg" src="${post.pic}" alt="">
+                <h3 class="postTitle">${post.title}</h3>
+                <p class="postText">${post.body}</p>
+                <p class="postHashtags">${post.hashtags}</p>
+                <div class="actions">
+                    <i class="fa-regular fa-thumbs-up"></i>
+                    <i class="fa-solid fa-share-nodes"></i>
+                </div>
+            </div>
             `
-        )
+        );
     }
-
-})
+});
