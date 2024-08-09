99ebc247ce6aa390ac5c710ab7be421532669639
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-09 09:37:02 +0300

    add default picture if the main is null

--- public/admin.js
+++ public/admin.js
@@ -3,30 +3,63 @@ axios.get('/api/getUser')
 .then((res) => {
     console.log(res.data);
     for(let user of res.data){
-        $('.usersContainer').prepend(
-            `
-                        <div class="user">
-                        <div class="icons">
-                            <i class="fa-regular fa-flag"></i>
-                            <i class="fa-solid fa-trash-can"></i>
-                        </div>
 
-                <div class="topInfo">
-                    <img src="${user.profilePicture}" alt="#" class="userPic">
-                    <div class="basicCon">
-                        <div class="nameCon">
-                            <span class="name">${user.firstname}</span>
-                            <span class="lastName">${user.lastName}</span>
-                        </div>
-                        <div class="email">${user.email}</div>
+        if(!user.profilePicture){
+            $('.usersContainer').prepend(
+                `
+                            <div class="user">
+                            <div class="icons">
+                                <i class="fa-regular fa-flag"></i>
+                                <i class="fa-solid fa-trash-can"></i>
+                            </div>
+    
+                    <div class="topInfo">
+                    <div class="userPicCon">
+                        <img src="./materials/profile pic default.png" alt="#" class="userPic">
+
                     </div>
+                        <div class="basicCon">
+                            <div class="nameCon">
+                                <span class="name">${user.firstname}</span>
+                                <span class="lastName">${user.lastName}</span>
+                            </div>
+                            <div class="email">${user.email}</div>
+                        </div>
+                    </div>
+                    <div class="description">${user.profileDescription}</div>
+                    <div class="visited">Visited: <span>${user.placesVisited}</span></div>
+                    <div class="toVisit">Wants to visit: <span>${user.placesToVisit}</span></div>
                 </div>
-                <div class="description">${user.profileDescription}</div>
-                <div class="visited">Visited: <span>${user.placesVisited}</span></div>
-                <div class="toVisit">Wants to visit: <span>${user.placesToVisit}</span></div>
-            </div>
-            `
-        )
+                `
+            )
+        }else{
+            $('.usersContainer').prepend(
+                `
+                            <div class="user">
+                            <div class="icons">
+                                <i class="fa-regular fa-flag"></i>
+                                <i class="fa-solid fa-trash-can"></i>
+                            </div>
+    
+                    <div class="topInfo">
+                    <div class="userPicCon">
+                        <img src="${user.profilePicture}" alt="#" class="userPic">
+                        </div>
+                        <div class="basicCon">
+                            <div class="nameCon">
+                                <span class="name">${user.firstname}</span>
+                                <span class="lastName">${user.lastName}</span>
+                            </div>
+                            <div class="email">${user.email}</div>
+                        </div>
+                    </div>
+                    <div class="description">${user.profileDescription}</div>
+                    <div class="visited">Visited: <span>${user.placesVisited}</span></div>
+                    <div class="toVisit">Wants to visit: <span>${user.placesToVisit}</span></div>
+                </div>
+                `
+            )
+        }
     }
 })
 .catch((err) => {
