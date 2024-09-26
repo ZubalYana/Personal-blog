20cd5562feb14614cac07ea2399852da8704f8f4
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-26 11:50:15 +0300

    delete proper theme changing logic

--- public/main.js
+++ public/main.js
@@ -635,50 +635,7 @@ setTimeout(() => {
 });
 
 //theme changing
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
 
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for(let i=0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function applyTheme(theme) {
-    if (theme === 'dark') {
-        $('body').addClass('dark-theme');
-        $('.themeChanger').text('dark');
-    } else {
-        $('body').removeClass('dark-theme');
-        $('.themeChanger').text('light');
-    }
-}
-
-$(document).ready(function() {
-    const savedTheme = getCookie('theme') || 'light'; 
-    applyTheme(savedTheme);
-});
-
-$('.themeChanger').click(function() {
-    const currentTheme = $('body').hasClass('dark-theme') ? 'dark' : 'light';
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-
-    applyTheme(newTheme);
-
-    setCookie('theme', newTheme, 30);
-});
 
 //settings popup logic
 $('.gear').click(() => {
